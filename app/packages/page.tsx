a65011464612a2d84f1ad498797952cdd7cf9d2c
ahmetulutas <ahmet.ulutas@ecozum.com>
2023-08-08 09:11:47 +0300

    bugfix: removed post package input

--- app/packages/page.tsx
+++ app/packages/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 
-import { generateDummyPackageItem } from "@/helpers/generateFakePackage";
+/* import { generateDummyPackageItem } from "@/helpers/generateFakePackage"; */
 
 export default function Packages() {
 
@@ -31,28 +31,28 @@ export default function Packages() {
         return;
     }
   };
-
-  const handlePost = async (e: React.FormEvent) => {
-    e.preventDefault();
-    const fakeData = generateDummyPackageItem();
-    if (!code) {
-      setError("Please fill in the password field");
-    }
-    else {
-      try {
-        setError("");
-        setLoading(true);
-        await fetch("/api/packages", {
-          method: "POST",
-          body: JSON.stringify({ ...fakeData, code })
-        });
-      } catch (err: any) {
-        throw new Error(err);
+  /* 
+    const handlePost = async (e: React.FormEvent) => {
+      e.preventDefault();
+      const fakeData = generateDummyPackageItem();
+      if (!code) {
+        setError("Please fill in the token field");
       }
-      setLoading(false);
-      setCode("");
-    };
-  };
+      else {
+        try {
+          setError("");
+          setLoading(true);
+          await fetch("/api/packages", {
+            method: "POST",
+            body: JSON.stringify({ ...fakeData, code })
+          });
+        } catch (err: any) {
+          throw new Error(err);
+        }
+        setLoading(false);
+        setCode("");
+      };
+    }; */
   const handleGetById = async (e: React.FormEvent) => {
     e.preventDefault();
     if (!packageId) {
@@ -75,16 +75,16 @@ export default function Packages() {
   return (
     <div className='flex flex-col gap-8 m-10'>
       <h1 className='text-center text-bold text-violet-900'>TEST PACKAGES ENDPOINTS</h1>
-      <form onSubmit={handlePost} className='flex flex-col gap-4'>
+      {/*       <form onSubmit={handlePost} className='flex flex-col gap-4'>
         <input name="password" type="password" className="border-2 border-gray-800 p-2" onChange={handleChange} placeholder='password' />
         <button className="bg-gray-100 hover:bg-gray-300 border-2 border-gray-800 p-2" type="submit">POST PACKAGE WITH RANDOM DATA</button>
-      </form>
+      </form> */}
       <div className='flex flex-col gap-4'>
         <button className="bg-gray-100 hover:bg-gray-300 border-2 border-gray-800 p-2" onClick={handleGetAllData}>TEST GET PACKAGES ENDPOINT</button>
         {bulkData ? <span className="inline-block mt-2 h-40 overflow-scroll">{JSON.stringify(bulkData)}</span> : null}
       </div>
       <form className='flex flex-col gap-4' onSubmit={handleGetById}>
-        <input name="packageId" type="text" className="border-2 border-gray-800 p-2" onChange={handleChange} placeholder='enter package id' />
+        <input value={packageId} name="packageId" type="text" className="border-2 border-gray-800 p-2" onChange={handleChange} placeholder='Enter Package Id' />
         <button className="bg-gray-100 hover:bg-gray-300 border-2 border-gray-800 p-2" type="submit">TEST GET PACKAGE BY ID</button>
         {singleData ? <span className="inline-block mt-2">{JSON.stringify(singleData)}</span> : null}
       </form>
