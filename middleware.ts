beb06fcde26ec68fb1ef0398359593f5bc80d0a5
ahmetulutas <ahmet.ulutas@ecozum.com>
2024-05-13 11:35:22 +0300

    fix: allowed headers

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,26 @@
-
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 import { verifyToken } from "./helpers/jwtHelpers";
 
 const authRoutes = ["/api/auth/sign-in", "/auth/sign-in", "/api/auth/sign-out"];
-const adminRoutes = ["/dashboard" ,"/api/users"];
+const adminRoutes = ["/dashboard", "/api/users"];
 
-export async function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest, res: NextResponse) {
   const { url, nextUrl, cookies } = request;
   const isAdminRoute = adminRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-  const isExternalApiCall = request.nextUrl.pathname.startsWith("/api");
+  const isApiCall = request.nextUrl.pathname.startsWith("/api");
   const { value: tokenInCookie } = cookies.get("token") ?? { value: null };
   const verifyTokenInCookie = await verifyToken(tokenInCookie ?? "");
 
-  if(isAuthRoute) {
+  if (request.method === "OPTIONS") {
+    return NextResponse.next();
+  }
+
+  if (isAuthRoute) {
     // routes that don't require token in the headers or cookie.
+
     cookies.delete("token");
     const requestHeaders = new Headers(request.headers);
     requestHeaders.set("Authorization", "");
@@ -27,38 +31,62 @@ export async function middleware(request: NextRequest) {
     });
   }
 
-  if(isExternalApiCall) {
+  if (isApiCall) {
     // Bearer token must be added for api calls..
     const requestHeaders = new Headers(request.headers);
-    const authorizationHeader = requestHeaders.get("authorization");
-    if(!authorizationHeader) return NextResponse.json({ message: "Unauthorized! Missing Authorization header." }, { status: 401 });
+    const authorizationHeader =
+      requestHeaders.get("authorization") ||
+      requestHeaders.get("Authorization");
 
-    if(authorizationHeader.startsWith("Bearer ")) {
+    if (request.method === "OPTIONS") {
+      return NextResponse.next();
+    }
+    if (!authorizationHeader)
+      return NextResponse.json(
+        { message: "Unauthorized! Missing Authorization header." },
+        { status: 401 }
+      );
+    if (authorizationHeader.startsWith("Bearer ")) {
       const token = authorizationHeader?.substring(7);
       const verifiedToken = await verifyToken(token);
 
-      if(!token || !verifiedToken) return NextResponse.json({ message: "Unauthorized! Missing Bearer token." }, { status: 401 });
-      if(isAdminRoute) {
+      if (!token || !verifiedToken)
+        return NextResponse.json(
+          { message: "Unauthorized! Missing Bearer token." },
+          { status: 401 }
+        );
+      if (isAdminRoute) {
         const { role } = verifiedToken;
-        if(!(role === "admin")) {
-          return NextResponse.json({ message: "Unauthorized! You are unauthorized" }, { status: 401 });
+        if (!(role === "admin")) {
+          return NextResponse.json(
+            { message: "Unauthorized! You are unauthorized" },
+            { status: 403 }
+          );
         }
       }
       return NextResponse.next();
     }
-
   }
 
-  if(!verifyTokenInCookie) {
+  if (!verifyTokenInCookie) {
     return NextResponse.redirect(new URL("/unauthorized", url));
   }
-  if(isAdminRoute) {
+  if (isAdminRoute) {
     const { role } = verifyTokenInCookie;
-    if(role === "admin") {
+    if (role === "admin") {
       return NextResponse.next();
     }
     return NextResponse.redirect(new URL("/unauthorized", url));
   }
+  return NextResponse.next();
 }
 
-export const config = { matcher: ["/api/:path*", "/packages" , "/dashboard", "/payment" , "/auth/sign-in"] };
+export const config = {
+  matcher: [
+    "/api/:path*",
+    "/packages",
+    "/dashboard",
+    "/payment",
+    "/auth/sign-in",
+  ],
+};
