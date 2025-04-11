67664beb27c361898eb681cd4f8926b82fb3ab34
Ahmet Ulutaş <82840617+ahmettulutas@users.noreply.github.com>
2025-04-11 12:22:49 +0300

    added detailed unathorized message

--- app/unauthorized/page.tsx
+++ app/unauthorized/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default async function Page() {
   return (
     <div className="flex w-full pt-20 items-center justify-center flex-col gap-4">
-      <h1>You are unauthorized to see this page. Please sign in first.</h1>
+      <h1>You are unauthorized to see this page. If you try to access dashboard page you need to be admin. To access other pages and obtain a bearer token to try api please sign in first.</h1>
       <Link className="border-2 border-gray-800 p-2" href="/auth/sign-in">
         SIGN IN
       </Link>
